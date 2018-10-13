eeb63acf73f1542836d77ab39ec89a2723b90e54
Benjamín Díaz <muimuiasd@gmail.com>
2018-10-12 21:16:41 -0300

    Primer commit
    Proyecto en Spring y carpeta de Base de datos

---
+++ SDSAA/src/main/java/cl/samuraidevelopers/sdsaa/SdsaaApplication.java
@@ -0,0 +1,12 @@
+package cl.samuraidevelopers.sdsaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsaaApplication.class, args);
+	}
+}
---
+++ SDSAA/src/main/java/cl/samuraidevelopers/sdsaa/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cl.samuraidevelopers.sdsaa;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SdsaaApplication.class);
+	}
+
+}
