4ab1e31074be4d7c731b58978f9f628f9b15e607
OscarBlaazz <44101549+OscarBlaazz@users.noreply.github.com>
2018-11-26 00:23:11 -0300

    Add files via upload

---
+++ EvaluacionRepository.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.repository;
+
+import java.util.Collection;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Evaluacion;
+
+public interface EvaluacionRepository {
+	Evaluacion findById(int id) throws DataAccessException;
+	
+	void save (Evaluacion evaluacion) throws DataAccessException;
+	
+	Collection<Evaluacion> findAll() throws DataAccessException;
+
+}
---
+++ JpaEvaluacionRepositorylmpl.java
@@ -0,0 +1,40 @@
+package org.springframework.samples.petclinic.repository.jpa;
+
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Evaluacion;
+import org.springframework.samples.petclinic.repository.EvaluacionRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("jpa")
+public class JpaEvaluacionRepositorylmpl implements EvaluacionRepository {
+	
+	
+	@PersistenceContext
+    private EntityManager ev;
+
+	@Override
+	public Evaluacion findById(int id) throws DataAccessException {
+		return this.ev.find(Evaluacion.class, id);
+	}
+
+	@Override
+	public void save(Evaluacion evaluacion) throws DataAccessException {
+		if(evaluacion.getId()==null) {
+			this.ev.persist(evaluacion);
+		}else {
+			this.ev.merge(evaluacion);
+		}
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public Collection<Evaluacion> findAll() throws DataAccessException {
+		return this.ev.createQuery("Select evaluacion from Evaluacion evaluacion").getResultList();
+	}
+}
