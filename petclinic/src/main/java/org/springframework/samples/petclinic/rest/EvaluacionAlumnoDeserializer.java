0aede2522e7b76927291ace3bd36bb8ec1a767c3
Camila <camilapinto@outlook.cl>
2018-12-05 13:06:49 -0300

    Evaluacion & EvaluacionAlumno GET

--- petclinic/src/main/java/org/springframework/samples/petclinic/rest/EvaluacionAlumnoDeserializer.java
+++ petclinic/src/main/java/org/springframework/samples/petclinic/rest/EvaluacionAlumnoDeserializer.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.rest;
 
 import java.io.IOException;
-
 import org.springframework.samples.petclinic.model.*;
 
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
@@ -11,6 +10,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@SuppressWarnings("serial")
 public class EvaluacionAlumnoDeserializer extends StdDeserializer<EvaluacionAlumno>{
 	
 	public EvaluacionAlumnoDeserializer() {
@@ -26,6 +26,7 @@ public class EvaluacionAlumnoDeserializer extends StdDeserializer<EvaluacionAlum
 		EvaluacionAlumno evaluacionAlumno = new EvaluacionAlumno();
 		Alumno alumno = new Alumno();
 		Evaluacion evaluacion = new Evaluacion(); 
+
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode node = parser.getCodec().readTree(parser);
 		JsonNode node_al = node.get("alumno");
@@ -39,6 +40,7 @@ public class EvaluacionAlumnoDeserializer extends StdDeserializer<EvaluacionAlum
 		if(!(evaluacionAlumnoId == 0)) {
 			evaluacionAlumno.setId(evaluacionAlumnoId);
 		}
+		
 		evaluacionAlumno.setNota(nota);
 		evaluacionAlumno.setAlumno(alumno);
 		evaluacionAlumno.setEvaluacion(evaluacion); 
--- petclinic/src/main/java/org/springframework/samples/petclinic/rest/EvaluacionAlumnoSerializer.java
+++ petclinic/src/main/java/org/springframework/samples/petclinic/rest/EvaluacionAlumnoSerializer.java
@@ -1,8 +1,6 @@
 package org.springframework.samples.petclinic.rest;
 
 import java.io.IOException;
-import java.text.Format;
-import java.text.SimpleDateFormat;
 
 import org.springframework.samples.petclinic.model.Alumno;
 import org.springframework.samples.petclinic.model.Evaluacion;
@@ -26,28 +24,27 @@ public class EvaluacionAlumnoSerializer extends StdSerializer<EvaluacionAlumno>{
 	
 	@Override
 	public void serialize(EvaluacionAlumno evaluacionAlumno, JsonGenerator jgen, SerializerProvider provider) throws IOException{
-		Format formatter = new SimpleDateFormat("yyyy/MM/dd");
 		jgen.writeStartObject();
+		
 		if(evaluacionAlumno.getId()==null) {
 			jgen.writeNullField("id");
 		}else {
 			jgen.writeNumberField("id", evaluacionAlumno.getId());
 		}
-		jgen.writeNumberField("nota", evaluacionAlumno.getNota());
 		
 		Alumno alumno = evaluacionAlumno.getAlumno();
+		Evaluacion evaluacion = evaluacionAlumno.getEvaluacion();
+		
 		jgen.writeObjectFieldStart("alumno");
 		jgen.writeNumberField("id", alumno.getId());
 		jgen.writeStringField("nombre", alumno.getNombre());
-		jgen.writeStringField("apellido",alumno.getApellido());
+		jgen.writeStringField("apellido", alumno.getApellido());
 		jgen.writeEndObject();
-		jgen.writeArrayFieldStart("alumno");
 		
-		Evaluacion evaluacion = evaluacionAlumno.getEvaluacion();
 		jgen.writeObjectFieldStart("evaluacion");
 		jgen.writeNumberField("id", evaluacion.getId());
-		//jgen.writeNumberField("fecha", evaluacion.getFecha());
-		
-
+		//aqui debería haber un jgen de fecha
+		jgen.writeEndObject();
+		jgen.writeEndObject();
 	}
 }
