15a567a0e3c92a47aed7b9cad3d6023a5144d308
OscarBlaazz <44101549+OscarBlaazz@users.noreply.github.com>
2018-12-05 17:41:36 -0300

    services

---
+++ petclinic/src/test/java/org/springframework/samples/petclinic/service/AbstractEvaluacionServiceTests.java
@@ -0,0 +1,62 @@
+package org.springframework.samples.petclinic.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Evaluacion;
+import org.springframework.transaction.annotation.Transactional;
+
+public  abstract class AbstractEvaluacionServiceTests {
+
+	@Autowired
+	protected EvaluacionService evaluacionService;
+	
+
+	protected AsignaturaService asignaturaService;
+	
+	@Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+	
+	@Test
+    public void shouldFindEvaluacionById() {
+    	Evaluacion evaluacion = this.evaluacionService.findEvaluacionById(1);
+    	assertThat(evaluacion.getId()).isEqualTo(1);
+    }
+	
+	
+	@Test
+    @Transactional
+    public void shouldSaveEvaluacion() {
+    	Collection<Evaluacion> evaluacion = this.evaluacionService.findAllEvaluacion();
+        int tamano = evaluacion.size();
+
+        Evaluacion eva = new Evaluacion();
+        eva.setFecha(new Date());
+        eva.setAsignatura(this.asignaturaService.findAsignaturaById(1));
+        this.evaluacionService.saveEvaluacion(eva);
+        assertThat(eva.getId().longValue()).isNotEqualTo(0);
+        assertThat(evaluacion.size()).isEqualTo(tamano + 1);
+    }
+	
+	@Test
+    @Transactional
+    public void shouldDeleteEvaluacion() {
+    	Evaluacion evaluacion = this.evaluacionService.findEvaluacionById(1);
+    	this.evaluacionService.deleteEvaluacion(evaluacion);
+    	try {
+    		evaluacion = this.evaluacionService.findEvaluacionById(1);
+    	} catch (Exception e) {
+    		evaluacion = null; 
+    	}
+    	assertThat(evaluacion).isNull(); 
+    }
+	
+}
